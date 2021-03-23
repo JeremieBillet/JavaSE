c97841a7d3ccce65a20698ab2befc28dcae892ce
Jeremie Billet <jeremiebillet@orange.fr>
2021-03-23 11:34:00 +0100

    création class client

---
+++ JBBank/src/com/hninstitut/appTest/Test.java
@@ -0,0 +1,10 @@
+package com.hninstitut.appTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ JBBank/src/com/hninstitut/components/Client.java
@@ -0,0 +1,48 @@
+package com.hninstitut.components;
+
+/**
+ * 1.1.1 Creation of the client class.
+ * 
+ * @author JB
+ *
+ */
+public class Client {
+
+	/*---------- Attributs -------------*/
+	private String name;
+	private String firstName;
+	private int clientNumber;
+	private static int numberOfClients;
+
+	/*---------- Constructeurs -------------*/
+	public Client(String name, String firstName) {
+		this.name = name;
+		this.firstName = firstName;
+		this.clientNumber = ++numberOfClients;
+
+	}
+
+	/*---------- Méthodes -------------*/
+	@Override
+	public String toString() {
+		return "Client [name=" + name + ", firstName=" + firstName + ", clientNumber=" + clientNumber + "]";
+	}
+
+	/*---------- Getters/Setters -------------*/
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
